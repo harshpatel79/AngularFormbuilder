ea41a4c850c44f4002f1789db9d8a9fe4d3b9ef9
harshpatel7962 <harshpatel7962@gmail.com>
2016-02-23 19:57:22 +0530

    updated 23th

--- js/controller.js
+++ js/controller.js
@@ -1,10 +1,34 @@
-myapp.controller('myformbuilder',['$scope','$timeout',function($scope,$timeout){
-	$scope.form=[];
+myapp.controller('myformbuilder',['$scope','$compile','$timeout',function($scope,$compile,$timeout){
+	  $scope.form=[];
     $scope.textbox="textbox";
     $scope.textArea="textArea";
     $scope.checkbox="checkbox";
     $scope.radiobutton="radiobutton";
     $scope.uniqueId = 0 ;
+    var El = angular.element(document.querySelector('.mydroppable'));
+    $scope.dropedFunction = function(event, ui){
+      var arrayDiv = [];
+      var component = $(ui.draggable).attr('component');
+      El.append($compile('<customform class="mycustomform" id="customform'+$scope.uniqueId+'" sort-Component="sortComponent" delete-Component="deleteComponent" component="'+component+'" unique-Id ="'+$scope.uniqueId+'"></customform>')($scope));
+      $scope.addComponent(component);
+    }
+    El.droppable({
+      accept: ".draggable",
+      drop:$scope.dropedFunction
+    });
+    var arrayDiv = [];
+    $("#sortable").sortable({
+      start: function(e, ui){
+          ui.placeholder.height(ui.item.height());
+      },
+      stop: function( event, ui ) {
+        $('.customform').each(function(){
+          arrayDiv.push($(this).attr('id').match(/\d+$/)[0]);
+        });
+        $scope.sortComponent(arrayDiv)
+        arrayDiv = [];
+      }
+    });
     $scope.addComponent = function(droppedcomponent){
         $scope.$apply(function(){
             var component = {};
@@ -25,16 +49,6 @@ myapp.controller('myformbuilder',['$scope','$timeout',function($scope,$timeout){
             }
             $scope.$apply();     
         }, 0);
-       //  // $scope.$apply(function(){
-       //      for(var i =0 ;i<$scope.form.length;i++){
-       //          if($scope.form[i].id == deleteId){
-       //              $scope.form.splice(i, 1);
-       //              break
-       //          }
-       //      }
-       // // },true);
-       // $scope.$apply();         
-
     };
     $scope.sortComponent = function(arrayDiv){
         var dummy = [];
--- js/directive.js
+++ js/directive.js
@@ -30,70 +30,47 @@ myapp.directive('formcomponents',[function () {
       }
     };
   }]);
-myapp.directive('formbuilder',['$compile',function ($compile) {
-    return {
-      template:'<div class="mydroppable " id="sortable"></div>',
+myapp.directive('customform',['$compile',function($compile){
+  var getTemplate = function(component,uniqueId){
+    var textbox = '<div class="customform" id="dropped'+uniqueId+'" ><span class="text">textbox</span><span class="close" id="close'+uniqueId+'" ng-click="close('+uniqueId+')">X</span><input type="text" name="fname"></div>';
+    var textArea = '<div class="customform" id="dropped'+uniqueId+'"><span class="text">textArea</span><span class="close" id="close'+uniqueId+'" ng-click="close('+uniqueId+')">X</span><textarea rows="4" cols="30"></textarea></div>';
+    var checkbox = '<div class="customform" id="dropped'+uniqueId+'"><span class="text">checkbox</span><span class="close" id="close'+uniqueId+'" ng-click="close('+uniqueId+')">X</span><input type="checkbox" name="vehicle" value="Bike"></div>';
+    var radiobutton = '<div class="customform" id="dropped'+uniqueId+'"><span class="text">radiobutton</span><span class="close" id="close'+uniqueId+'" ng-click="close('+uniqueId+')">X</span><input type="radio" name="gender" value="male" checked> Male<br><input type="radio" name="gender" value="female"> Female<br><input type="radio" name="gender" value="other"> Other<br/></div>';
+    var template = '';
+        switch(component) {
+            case 'textbox':
+                template = textbox;
+                break;
+            case 'textArea':
+                template = textArea;
+                break;
+            case 'checkbox':
+                template = checkbox;
+                break;
+            case 'radiobutton':
+                template = radiobutton;
+                break;
+        }
+        return template;
+  }
+  return{
       restrict: 'E',
-      multiElement: true,
       transclude: true,
       replace:true,
       scope: {
-              accepts: "=",
-              addComponent: "&",
-              form: "=",
-              uniqueId: "=",
-              deleteComponent: "&",
-              sortComponent:"&"
-          },
+        deleteComponent: "&",
+        sortComponent:"&"
+            },
       link:function(scope, element, attr){
-            var reference = scope.addComponent();
+            scope.component = attr.component;
+            scope.divuniqueId = attr.uniqueId;
             var deleteReference = scope.deleteComponent();
             var sortComponent =scope.sortComponent();
             scope.close = function(e){
-              //alert(e);
-              angular.element(element[0].querySelector('#dropped'+e)).remove();
               deleteReference(e);
+              angular.element(element[0].querySelector('#dropped'+e)).parent().remove();
             }
-            scope.dropedFunction = function(event, ui){
-              var arrayDiv = [];
-              var component = $(ui.draggable).attr('component');
-                if(component == "textbox"){
-                 element.append($compile('<div class="customform" id="dropped'+scope.uniqueId+'" ><span class="text">textbox</span><span class="close" id="close'+scope.uniqueId+'" ng-click="close('+scope.uniqueId+')">X</span><input type="text" name="fname"></div>')(scope));
-                }
-                else if(component == "textArea"){
-                 element.append($compile('<div class="customform" id="dropped'+scope.uniqueId+'"><span class="text">textArea</span><span class="close" id="close'+scope.uniqueId+'" ng-click="close('+scope.uniqueId+')">X</span><textarea rows="4" cols="30"></textarea></div>')(scope));
-                }
-                else if(component == "checkbox"){
-                 element.append($compile('<div class="customform" id="dropped'+scope.uniqueId+'"><span class="text">checkbox</span><span class="close" id="close'+scope.uniqueId+'" ng-click="close('+scope.uniqueId+')">X</span><input type="checkbox" name="vehicle" value="Bike"></div>')(scope));
-                }
-                else if(component == "radiobutton"){
-                 element.append($compile('<div class="customform" id="dropped'+scope.uniqueId+'"><span class="text">radiobutton</span><span class="close" id="close'+scope.uniqueId+'" ng-click="close('+scope.uniqueId+')">X</span><input type="radio" name="gender" value="male" checked> Male<br><input type="radio" name="gender" value="female"> Female<br><input type="radio" name="gender" value="other"> Other<br/></div>')(scope));
-                }
-
-                // angular.element(element[0].querySelector('#close'+scope.uniqueId)).bind('click',function($event){
-                //   var Id = $($event.target).attr('id').match(/\d+$/)[0];
-                //   deleteReference(Id);
-                //   angular.element(element[0].querySelector('#dropped'+Id)).remove();
-                // });
-                reference(component);
-                $("#sortable").sortable({
-                  start: function(e, ui){
-                      ui.placeholder.height(ui.item.height());
-                  },
-                  stop: function( event, ui ) {
-                    $('.customform').each(function(){
-                      arrayDiv.push($(this).attr('id').match(/\d+$/)[0]);
-                    });
-                    sortComponent(arrayDiv)
-                   // console.log(arrayDiv);
-                    arrayDiv = [];
-                  }
-                });
-            }
-            element.droppable({
-              accept: ".draggable",
-              drop:scope.dropedFunction
-            });
+            element.append($compile(getTemplate(attr.component,attr.uniqueId))(scope));
       }
-    };
-  }]);
+  }
+}]);
